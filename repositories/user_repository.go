6a30a8a62214714eb4c506c3bbdee142252fdbde
zhaoheng <1798835790@qq.com>
2020-05-01 17:29:00 +0800

    用户repository

---
+++ repositories/user_repository.go
@@ -0,0 +1,102 @@
+package repositories
+
+import (
+	"database/sql"
+	"shop-iris/common"
+	"shop-iris/datamodels"
+	"strconv"
+
+	"github.com/kataras/iris/core/errors"
+)
+
+type IUserRepository interface {
+	Conn() error
+	Select(userName string) (user *datamodels.User, err error)
+	//插入,只要传入user结构体即可
+	Insert(user *datamodels.User) (userId int64, err error)
+}
+
+func NewUserRepository(table string, db *sql.DB) IUserRepository {
+	return &UserManagerRepository{table, db}
+}
+
+type UserManagerRepository struct {
+	table     string
+	mysqlConn *sql.DB
+}
+
+func (u *UserManagerRepository) Conn() (err error) {
+	if u.mysqlConn == nil {
+		mysql, errMysql := common.NewMysqlConn()
+		if errMysql != nil {
+			return errMysql
+		}
+		u.mysqlConn = mysql
+	}
+	if u.table == "" {
+		u.table = "user"
+	}
+	return
+}
+
+func (u *UserManagerRepository) Select(userName string) (user *datamodels.User, err error) {
+	if userName == "" {
+		return &datamodels.User{}, errors.New("条件不能为空！")
+	}
+	if err = u.Conn(); err != nil {
+		return &datamodels.User{}, err
+	}
+
+	sql := "Select * from " + u.table + " where userName=?"
+	rows, errRows := u.mysqlConn.Query(sql, userName)
+	defer rows.Close()
+	if errRows != nil {
+		return &datamodels.User{}, errRows
+	}
+
+	result := common.GetResultRow(rows)
+	if len(result) == 0 {
+		return &datamodels.User{}, errors.New("用户不存在！")
+	}
+
+	user = &datamodels.User{}
+	common.DataToStructByTagSql(result, user)
+	return
+}
+
+func (u *UserManagerRepository) Insert(user *datamodels.User) (userId int64, err error) {
+	if err = u.Conn(); err != nil {
+		return
+	}
+
+	sql := "INSERT " + u.table + " SET nickName=?,userName=?,passWord=?"
+	stmt, errStmt := u.mysqlConn.Prepare(sql)
+	defer stmt.Close()
+	if errStmt != nil {
+		return userId, errStmt
+	}
+	result, errResult := stmt.Exec(user.NickName, user.UserName, user.HashPassword)
+	if errResult != nil {
+		return userId, errResult
+	}
+	return result.LastInsertId()
+}
+
+func (u *UserManagerRepository) SelectByID(userId int64) (user *datamodels.User, err error) {
+	if err = u.Conn(); err != nil {
+		return &datamodels.User{}, err
+	}
+	sql := "select * from " + u.table + " where ID=" + strconv.FormatInt(userId, 10)
+	row, errRow := u.mysqlConn.Query(sql)
+	defer row.Close()
+	if errRow != nil {
+		return &datamodels.User{}, errRow
+	}
+	result := common.GetResultRow(row)
+	if len(result) == 0 {
+		return &datamodels.User{}, errors.New("用户不存在！")
+	}
+	user = &datamodels.User{}
+	common.DataToStructByTagSql(result, user)
+	return
+}
