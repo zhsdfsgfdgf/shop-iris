b36b121b2dd45cca6e93402585b1e53863c6cbf0
zhaoheng <1798835790@qq.com>
2020-05-02 16:39:18 +0800

    完善mysql连接

--- common/mysql.go
+++ common/mysql.go
@@ -6,9 +6,24 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
+// 定义一个全局对象db
+var db *sql.DB
+
 //创建mysql 连接
 func NewMysqlConn() (db *sql.DB, err error) {
-	db, err = sql.Open("mysql", "root:zhaoheng@tcp(47.105.83.40:3306)/shop-iris?charset=utf8")
+	// DSN:Data Source Name
+	dsn := "root:zhaoheng@tcp(47.105.83.xx:3306)/shop-iris?charset=utf8"
+	// 不会校验账号密码是否正确
+	// 注意！！！这里不要使用:=，我们是给全局变量赋值，然后在main函数中使用全局变量db
+	db, err = sql.Open("mysql", dsn)
+	if err != nil {
+		return
+	}
+	// 尝试与数据库建立连接（校验dsn是否正确）
+	err = db.Ping()
+	if err != nil {
+		return
+	}
 	return
 }
 
