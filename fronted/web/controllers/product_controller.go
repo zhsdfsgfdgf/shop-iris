6514683699315e4b675aca5441cfd28ec5c51541
zhaoheng <1798835790@qq.com>
2020-05-02 19:07:51 +0800

    商品详情页控制器

---
+++ fronted/web/controllers/product_controller.go
@@ -0,0 +1,30 @@
+package controllers
+
+import (
+	"shop-iris/services"
+
+	"github.com/kataras/iris"
+	"github.com/kataras/iris/mvc"
+	"github.com/kataras/iris/sessions"
+)
+
+type ProductController struct {
+	Ctx            iris.Context
+	ProductService services.IProductService
+	Session        *sessions.Session
+}
+
+func (p *ProductController) GetDetail() mvc.View {
+	product, err := p.ProductService.GetProductByID(1)
+	if err != nil {
+		p.Ctx.Application().Logger().Error(err)
+	}
+
+	return mvc.View{
+		Layout: "shared/productLayout.html",
+		Name:   "product/view.html",
+		Data: iris.Map{
+			"product": product,
+		},
+	}
+}
