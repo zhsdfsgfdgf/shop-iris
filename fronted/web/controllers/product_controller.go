9b15c5b81371390b9d6d7732319ae82325c9eeb0
zhaoheng <1798835790@qq.com>
2020-05-03 14:24:27 +0800

    减库存,下单

--- fronted/web/controllers/product_controller.go
+++ fronted/web/controllers/product_controller.go
@@ -1,7 +1,10 @@
 package controllers
 
 import (
+	"fmt"
+	"shop-iris/datamodels"
 	"shop-iris/services"
+	"strconv"
 
 	"github.com/kataras/iris"
 	"github.com/kataras/iris/mvc"
@@ -11,6 +14,7 @@ import (
 type ProductController struct {
 	Ctx            iris.Context
 	ProductService services.IProductService
+	OrderService   services.IOrderService
 	Session        *sessions.Session
 }
 
@@ -26,4 +30,56 @@ func (p *ProductController) GetDetail() mvc.View {
 			"product": product,
 		},
 	}
+}
+func (p *ProductController) GetOrder() mvc.View {
+	productString := p.Ctx.URLParam("productID")
+	userString := p.Ctx.GetCookie("uid")
+	productID, err := strconv.Atoi(productString)
+	if err != nil {
+		p.Ctx.Application().Logger().Debug(err)
+	}
+	product, err := p.ProductService.GetProductByID(int64(productID))
+	if err != nil {
+		p.Ctx.Application().Logger().Debug(err)
+	}
+	var orderID int64
+	showMessage := "抢购失败！"
+	//判断商品数量是否满足需求
+	if product.ProductNum > 0 {
+		//扣除商品数量
+		product.ProductNum -= 1
+		err := p.ProductService.UpdateProduct(product)
+		if err != nil {
+			p.Ctx.Application().Logger().Debug(err)
+		}
+		//创建订单
+		userID, err := strconv.Atoi(userString)
+		if err != nil {
+			p.Ctx.Application().Logger().Debug(err)
+		}
+
+		order := &datamodels.Order{
+			UserId:      int64(userID),
+			ProductId:   int64(productID),
+			OrderStatus: datamodels.OrderSuccess,
+		}
+		fmt.Println(order)
+		//新建订单
+		orderID, err = p.OrderService.InsertOrder(order)
+		if err != nil {
+			p.Ctx.Application().Logger().Debug(err)
+		} else {
+			showMessage = "抢购成功！"
+		}
+	}
+
+	return mvc.View{
+		Layout: "shared/productLayout.html",
+		Name:   "product/result.html",
+		Data: iris.Map{
+			"orderID":     orderID,
+			"showMessage": showMessage,
+		},
+	}
+
 }
