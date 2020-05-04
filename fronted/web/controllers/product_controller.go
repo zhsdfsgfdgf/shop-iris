afe42ae603969fa48b8bde254148b1d726dd9b9b
zhaoheng <1798835790@qq.com>
2020-05-04 11:52:06 +0800

    编写控制器-生成指定商品静态文件

--- fronted/web/controllers/product_controller.go
+++ fronted/web/controllers/product_controller.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"os"
+	"path/filepath"
 	"shop-iris/datamodels"
 	"shop-iris/services"
 	"strconv"
@@ -14,14 +15,15 @@ import (
 
 var (
 	//生成的Html保存目录
-	htmlOutPath = "./fronted/web/htmlProductShow/"
+	htmlOutPath = "./web/htmlProductShow/"
 	//静态文件模版目录
-	templatePath = "./fronted/web/views/template/"
+	templatePath = "./web/views/template/"
 )
 
 //生成html静态文件
 //template是go自带的模板文件
 func generateStaticHtml(ctx iris.Context, template *template.Template, fileName string, product *datamodels.Product) {
+
 	//1.判断静态文件是否存在
 	if exist(fileName) {
 		err := os.Remove(fileName)
@@ -30,7 +32,7 @@ func generateStaticHtml(ctx iris.Context, template *template.Template, fileName 
 		}
 	}
 	//2.生成静态文件
-	file, err := os.OpenFile(fileName, os.O_CREATE, os.ModePerm)
+	file, err := os.OpenFile(fileName, os.O_CREATE|os.O_WRONLY, os.ModePerm)
 	if err != nil {
 		ctx.Application().Logger().Error(err)
 	}
@@ -43,6 +45,31 @@ func generateStaticHtml(ctx iris.Context, template *template.Template, fileName 
 func exist(fileName string) bool {
 	_, err := os.Stat(fileName)
 	return err == nil || os.IsExist(err)
+}
+
+func (p *ProductController) GetGenerateHtml() {
+	//http://127.0.0.1:8082/product/generate/html?productID=3
+	productString := p.Ctx.URLParam("productID")
+	productID, err := strconv.Atoi(productString)
+	if err != nil {
+		p.Ctx.Application().Logger().Debug(err)
+	}
+	//1.获取模版
+	contenstTmp, err := template.ParseFiles(filepath.Join(templatePath, "product.html"))
+	if err != nil {
+		p.Ctx.Application().Logger().Debug(err)
+	}
+
+	//2.获取html生成路径
+	fileName := filepath.Join(htmlOutPath, "htmlProduct.html")
+
+	//3.获取模版渲染数据
+	product, err := p.ProductService.GetProductByID(int64(productID))
+	if err != nil {
+		p.Ctx.Application().Logger().Debug(err)
+	}
+	//4.生成静态文件
+	generateStaticHtml(p.Ctx, contenstTmp, fileName, product)
 }
 
 type ProductController struct {
