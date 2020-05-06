9b2d02637ee131f2be0233b1aa09975c958c48c3
zhaoheng <1798835790@qq.com>
2020-05-06 15:22:42 +0800

    完善aes加密文件

--- encrypt/aes.go
+++ encrypt/aes.go
@@ -4,8 +4,11 @@ import (
 	"bytes"
 	"crypto/aes"
 	"crypto/cipher"
-	"fmt"
+	"encoding/base64"
+	"errors"
 )
+
+//高级加密标准（Adevanced Encryption Standard ,AES）
 
 //16,24,32位字符串的话，分别对应AES-128，AES-192，AES-256 加密方法
 //key不能泄露,泄露的话会根据我们在客户端的加密串进行解密,就会知道我们加密串里的内容是什么,也可以隔一段时间改一下,然后重新编译
@@ -18,8 +21,21 @@ func PKCS7Padding(ciphertext []byte, blockSize int) []byte {
 	padding := blockSize - len(ciphertext)%blockSize
 	//Repeat()函数的功能是把切片[]byte{byte(padding)}复制padding个，然后合并成新的字节切片返回
 	padtext := bytes.Repeat([]byte{byte(padding)}, padding)
-	fmt.Println(padtext)
 	return append(ciphertext, padtext...)
+}
+
+//填充的反向操作，删除填充字符串
+func PKCS7UnPadding(origData []byte) ([]byte, error) {
+	//获取数据长度
+	length := len(origData)
+	if length == 0 {
+		return nil, errors.New("加密字符串错误！")
+	} else {
+		//获取填充字符串长度,aes会将字符串长度赋在后面
+		unpadding := int(origData[length-1])
+		//截取切片，删除填充字节，并且返回明文
+		return origData[:(length - unpadding)], nil
+	}
 }
 
 //实现加密
@@ -39,4 +55,35 @@ func AesEcrypt(origData []byte, key []byte) ([]byte, error) {
 	//执行加密
 	blocMode.CryptBlocks(crypted, origData)
 	return crypted, nil
+}
+
+//实现解密
+func AesDeCrypt(cypted []byte, key []byte) ([]byte, error) {
+	//创建加密算法实例
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+	//获取块大小
+	blockSize := block.BlockSize()
+	//创建加密客户端实例
+	blockMode := cipher.NewCBCDecrypter(block, key[:blockSize])
+	origData := make([]byte, len(cypted))
+	//这个函数也可以用来解密
+	blockMode.CryptBlocks(origData, cypted)
+	//去除填充字符串
+	origData, err = PKCS7UnPadding(origData)
+	if err != nil {
+		return nil, err
+	}
+	return origData, err
+}
+
+//加密后数据类型是切片,将其转换成string,然后保存到cookie里
+func EnPwdCode(pwd []byte) (string, error) {
+	result, err := AesEcrypt(pwd, PwdKey)
+	if err != nil {
+		return "", err
+	}
+	return base64.StdEncoding.EncodeToString(result), err
 }
