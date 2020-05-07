d95b3fa1b0d977149a1ed2692bb530761ac72b37
zhaoheng <1798835790@qq.com>
2020-05-07 16:56:18 +0800

    拦截器添加身份验证

--- validate.go
+++ validate.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"net/http"
 	"shop-iris/common"
+	"shop-iris/encrypt"
 )
 
 //执行正常业务逻辑
@@ -16,12 +17,52 @@ func Check(w http.ResponseWriter, r *http.Request) {
 //统一验证拦截器，每个接口都需要提前验证
 func Auth(w http.ResponseWriter, r *http.Request) error {
 	fmt.Println("拦截器进行验证")
-	return errors.New("验证失败")
+	//添加基于cookie的权限认证
+	err := CheckUserInfo(r)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
+//身份校验函数
+func CheckUserInfo(r *http.Request) error {
+	//获取Uid，cookie
+	uidCookie, err := r.Cookie("uid")
+	if err != nil {
+		return errors.New("用户UID Cookie 获取失败！")
+	}
+	//获取用户加密串
+	signCookie, err := r.Cookie("sign")
+	if err != nil {
+		return errors.New("用户加密串 Cookie 获取失败！")
+	}
+
+	//对信息进行解密
+	signByte, err := encrypt.DePwdCode(signCookie.Value)
+	if err != nil {
+		return errors.New("加密串已被篡改！")
+	}
+
+	fmt.Println("结果比对")
+	fmt.Println("用户ID：" + uidCookie.Value)
+	fmt.Println("解密后用户ID：" + string(signByte))
+	if checkInfo(uidCookie.Value, string(signByte)) {
+		return nil
+	}
+	return errors.New("身份校验失败！")
+}
+
+//自定义逻辑判断
+func checkInfo(checkStr string, signStr string) bool {
+	if checkStr == signStr {
+		return true
+	}
+	return false
+}
 func main() {
 
-	//1、过滤器
+	//1、拦截器
 	filter := common.NewFilter()
 	//注册拦截器,每次访问check接口,都会使用拦截器
 	filter.RegisterFilterUri("/check", Auth)
