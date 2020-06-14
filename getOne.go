5180b45048bffe4b9c07465cd590d63f8e902c79
zhaoheng <1798835790@qq.com>
2020-06-14 16:31:45 +0800

    随机拒绝

--- getOne.go
+++ getOne.go
@@ -10,21 +10,27 @@ import (
 var sum int64 = 0
 
 //预存商品数量
-var productNum int64 = 1000
+var productNum int64 = 1000000
 
 //互斥锁
 var mutex sync.Mutex
+
+//计数
+var count int64 = 0
 
 //获取秒杀商品
 func GetOneProduct() bool {
 	//加锁
 	mutex.Lock()
 	defer mutex.Unlock()
+	count += 1
 	//判断数据是否超限
-	if sum < productNum {
-		sum += 1
-		fmt.Println(sum)
-		return true
+	if count%100 == 0 {
+		if sum < productNum {
+			sum += 1
+			fmt.Println(sum)
+			return true
+		}
 	}
 	return false
 
