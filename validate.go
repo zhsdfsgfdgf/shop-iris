5a1f611f9b0618d57793293bec59418a4f9e156e
zhaoheng <1798835790@qq.com>
2020-06-14 16:24:41 +0800

    添加集群节点

--- validate.go
+++ validate.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"net/url"
 	"shop-iris/common"
+	"shop-iris/datamodels"
 	"shop-iris/encrypt"
+	"shop-iris/rabbitmq"
 	"strconv"
 	"sync"
 )
@@ -20,7 +24,15 @@ var localHost = "127.0.0.1"
 
 var port = "8081"
 
+//数量控制接口服务器内网IP，或者getone的SLB内网IP
+var GetOneIp = "127.0.0.1"
+
+var GetOnePort = "8084"
+
 var hashConsistent *common.Consistent
+
+//rabbitmq
+var rabbitMqValidate *rabbitmq.RabbitMQ
 
 //用来存放控制信息
 type AccessControl struct {
@@ -47,12 +59,6 @@ func (m *AccessControl) SetNewRecord(uid int) {
 	m.RWMutex.Lock()
 	m.sourcesArray[uid] = "zhaoheng"
 	m.RWMutex.Unlock()
-}
-
-//执行正常业务逻辑
-func Check(w http.ResponseWriter, r *http.Request) {
-	//执行正常业务逻辑
-	fmt.Println("验证成功,执行check")
 }
 
 //获取分布式共享数据
@@ -97,42 +103,11 @@ func (m *AccessControl) GetDataFromMap(uid string) (isOk bool) {
 
 //获取其它节点处理结果
 func GetDataFromOtherMap(host string, request *http.Request) bool {
-	//获取Uid
-	uidPre, err := request.Cookie("uid")
+	hostUrl := "http://" + host + ":" + port + "/checkRight"
+	response, body, err := GetCurl(hostUrl, request)
 	if err != nil {
 		return false
 	}
-	//获取sign
-	uidSign, err := request.Cookie("sign")
-	if err != nil {
-		return false
-	}
-
-	//模拟接口访问，
-	client := &http.Client{}
-	req, err := http.NewRequest("GET", "http://"+host+":"+port+"/check", nil)
-	if err != nil {
-		return false
-	}
-
-	//基于cookie验证的核心
-	//手动指定，排查多余cookies
-	cookieUid := &http.Cookie{Name: "uid", Value: uidPre.Value, Path: "/"}
-	cookieSign := &http.Cookie{Name: "sign", Value: uidSign.Value, Path: "/"}
-	//添加cookie到模拟的请求中
-	req.AddCookie(cookieUid)
-	req.AddCookie(cookieSign)
-
-	//获取返回结果
-	response, err := client.Do(req)
-	if err != nil {
-		return false
-	}
-	body, err := ioutil.ReadAll(response.Body)
-	if err != nil {
-		return false
-	}
-
 	//判断状态
 	if response.StatusCode == 200 {
 		if string(body) == "true" {
@@ -183,6 +158,89 @@ func CheckUserInfo(r *http.Request) error {
 	return errors.New("身份校验失败！")
 }
 
+//执行正常业务逻辑
+func Check(w http.ResponseWriter, r *http.Request) {
+	//执行正常业务逻辑
+	fmt.Println("执行check！")
+	queryForm, err := url.ParseQuery(r.URL.RawQuery)
+	if err != nil || len(queryForm["productID"]) <= 0 {
+		w.Write([]byte("false"))
+		return
+	}
+	productString := queryForm["productID"][0]
+	fmt.Println(productString)
+	//获取用户cookie,用来获取userid
+	userCookie, err := r.Cookie("uid")
+	if err != nil {
+		w.Write([]byte("false"))
+		return
+	}
+
+	//1.分布式权限验证
+	right := accessControl.GetDistributedRight(r)
+	if right == false {
+		w.Write([]byte("false"))
+		return
+	}
+	//2.获取数量控制权限，防止秒杀出现超卖现象
+	hostUrl := "http://" + GetOneIp + ":" + GetOnePort + "/getOne"
+	responseValidate, validateBody, err := GetCurl(hostUrl, r)
+	if err != nil {
+		w.Write([]byte("false"))
+		return
+	}
+	//判断数量控制接口请求状态
+	if responseValidate.StatusCode == 200 {
+		if string(validateBody) == "true" {
+			//整合下单
+			//1.获取商品ID
+			productID, err := strconv.ParseInt(productString, 10, 64)
+			if err != nil {
+
+				w.Write([]byte("false"))
+				return
+			}
+			//2.获取用户ID
+			userID, err := strconv.ParseInt(userCookie.Value, 10, 64)
+			if err != nil {
+
+				w.Write([]byte("false"))
+				return
+			}
+
+			//3.创建消息体
+			message := datamodels.NewMessage(userID, productID)
+			//类型转化
+			byteMessage, err := json.Marshal(message)
+			if err != nil {
+				w.Write([]byte("false"))
+				return
+			}
+			//4.生产消息
+			err = rabbitMqValidate.PublishSimple(string(byteMessage))
+			if err != nil {
+				w.Write([]byte("false"))
+				return
+			}
+			w.Write([]byte("true"))
+			return
+		}
+	}
+	w.Write([]byte("false"))
+	return
+}
+
+//这样权限验证就不会重复执行发送的请求了
+func CheckRight(w http.ResponseWriter, r *http.Request) {
+	right := accessControl.GetDistributedRight(r)
+	if !right {
+		w.Write([]byte("false"))
+		return
+	}
+	w.Write([]byte("true"))
+	return
+}
+
 //自定义逻辑判断
 func checkInfo(checkStr string, signStr string) bool {
 	if checkStr == signStr {
@@ -190,14 +248,62 @@ func checkInfo(checkStr string, signStr string) bool {
 	}
 	return false
 }
-func main() {
 
+//模拟请求
+func GetCurl(hostUrl string, request *http.Request) (response *http.Response, body []byte, err error) {
+	//获取Uid
+	uidPre, err := request.Cookie("uid")
+	if err != nil {
+		return
+	}
+	//获取sign
+	uidSign, err := request.Cookie("sign")
+	if err != nil {
+		return
+	}
+
+	//模拟接口访问，
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", hostUrl, nil)
+	if err != nil {
+		return
+	}
+
+	//手动指定，排查多余cookies
+	cookieUid := &http.Cookie{Name: "uid", Value: uidPre.Value, Path: "/"}
+	cookieSign := &http.Cookie{Name: "sign", Value: uidSign.Value, Path: "/"}
+	//添加cookie到模拟的请求中
+	req.AddCookie(cookieUid)
+	req.AddCookie(cookieSign)
+
+	//获取返回结果
+	response, err = client.Do(req)
+	defer response.Body.Close()
+	if err != nil {
+		return
+	}
+	body, err = ioutil.ReadAll(response.Body)
+	return
+}
+
+func main() {
+	//采用一致性哈希算法
+	hashConsistent = common.NewConsistent()
+	//采用一致性hash算法，添加节点
+	for _, v := range hostArray {
+		hashConsistent.Add(v)
+	}
+	localIp, err := common.GetIntranceIp()
+	rabbitMqValidate = rabbitmq.NewRabbitMQSimple("shopProduct")
+	defer rabbitMqValidate.Destory()
 	//1、拦截器
 	filter := common.NewFilter()
 	//注册拦截器,每次访问check接口,都会使用拦截器
 	filter.RegisterFilterUri("/check", Auth)
-	//2、启动服务
+	filter.RegisterFilterUri("/checkRight", Auth)
+	//2、启动服务,被拦截器拦截,则执行不到
 	http.HandleFunc("/check", filter.Handle(Check))
+	http.HandleFunc("/checkRight", filter.Handle(CheckRight))
 	//启动服务
 	http.ListenAndServe(":8083", nil)
 }
